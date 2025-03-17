7f116a899023180a57264cce1bb4801fa3cea2fb
dsevar1 <dsevar00@gmail.com>
2025-03-17 19:06:39 +0100

    "Added a new class"

---
+++ NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiS_Vj1
+{
+    internal class NewClass
+    {
+    }
+}
